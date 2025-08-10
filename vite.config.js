757e81a8788d9761f8058808a59b84e95bf72c0f
Bruno Fragoso <fragoso@MacBook-Air-de-Bruno.local>
2025-08-10 15:11:53 -0300

    Ajustei o build para gerar múltiplas entradas HTML.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: { port: 5173 },
-  build: { target: 'es2018' },
+  build: {
+    target: 'es2018',
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        conteudo: resolve(__dirname, 'conteudo.html'),
+        templates: resolve(__dirname, 'templates.html'),
+        curso: resolve(__dirname, 'curso.html'),
+        termos: resolve(__dirname, 'termos.html'),
+        privacidade: resolve(__dirname, 'privacidade.html')
+      }
+    }
+  },
   preview: {
     allowedHosts: ['www.iaxplor.com', 'iaxplor.com'],
     host: '0.0.0.0',

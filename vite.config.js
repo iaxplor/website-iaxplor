a672cf5f1c9112fd16087856e9441d2dbf048d55
Bruno Fragoso <fragoso@MacBook-Air-de-Bruno.local>
2025-08-10 14:55:19 -0300

    ajuste no vite preview para permitir nosso domínio

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: { port: 5173 },
-  build: { target: 'es2018' }
+  build: { target: 'es2018' },
+  preview: {
+    allowedHosts: ['www.iaxplor.com', 'iaxplor.com'],
+    host: '0.0.0.0',
+  }
 });
 
